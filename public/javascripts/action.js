1cd0794a03013a4bea88adc7ca598558895be9be
Daniel Sellers <daniel@designfrontier.net>
2013-05-14 15:38:30 -0600

    Moved some things around and added...
    The silence function. Cleaned up some other things so that it is working
    as expected now.

--- public/javascripts/action.js
+++ public/javascripts/action.js
@@ -1,77 +1,90 @@
-var action = {
-	init: function(){
-		//TODO list
-		//	1. provide scoping for passed in functions
-		//	2. silence (remove function)
-		//	3. add in emitterID
-		//	4. once then silent listeners
-		//	5. constructor function for functions
-		//	6. trace function
-		//	7. move the proto mods into the constructor
+//TODO list
+//	1. provide scoping for passed in functions
+//	3. add in emitterID
+//	4. once then silent listeners
+//	5. constructor function for functions
+//	6. trace function
+//	7. move the proto mods into the constructor
+//	8. Private events module (local to the object)
 
-		var that = this;
+var action = function(){
+	var action = {
+		newFunction: function(functionIn){
+			var returnFunction = functionIn
+				, localEvents = {};
 
-		Function.prototype.emit = function(eventNameIn, eventDataIn){
-			//add the emitterID to this thing
-			var eventStack
-				, functionToCall
-				, i;
+			returnFunction.emitterId = Math.ceil(Math.random() * 10000);
 
-			if(typeof eventDataIn !== 'undefined'){
-				//we have some event data
-				eventData.payload = eventDataIn;
-			}
+			returnFunction.emit = function(eventNameIn, eventDataIn){
+				//add the emitterID to this thing
+				var eventStack
+					, functionToCall
+					, i;
 
-			//emit the event
-			if(typeof that.eventStore[eventNameIn] !== 'undefined'){
-				eventStack = that.eventStore[eventNameIn];
-
-				for(i = 0; i < eventStack.length; i ++){
-					eventStack[i](eventDataIn, this.emitterId);
+				if(typeof eventDataIn !== 'undefined'){
+					//we have some event data
+					eventData.payload = eventDataIn;
 				}
-			}
-		};
 
-		Function.prototype.listen = function(eventName, functionIn){
-			//check to see if we are getting an object or a number in the data
-			//	if it is just a number then we are dealing with the
-			//	emitterID only.
-			var i
-				, newCheck = true
-				, eventStack;
+				//emit the event
+				if(typeof action.eventStore[eventNameIn] !== 'undefined'){
+					eventStack = action.eventStore[eventNameIn];
 
-			if(typeof that.eventStore[eventName] !== 'undefined'){
-				//already exists check to see if the function is already bound
-				eventStack = that.eventStore[eventName];
-
-				for(i = 0; i < eventStack.length; i ++){
-					if(eventStack[i] === functionIn){
-						newCheck = false;
-						break;
+					for(i = 0; i < eventStack.length; i ++){
+						eventStack[i](eventDataIn, this.emitterId);
 					}
 				}
+			};
 
-				console.log(newCheck);
+			returnFunction.listen = function(eventName, functionIn){
+				//check to see if we are getting an object or a number in the data
+				//	if it is just a number then we are dealing with the
+				//	emitterID only.
+				var i
+					, newCheck = true
+					, eventStack;
 
-				if(newCheck){
-					eventStack.push(functionIn);
+				if(typeof action.eventStore[eventName] !== 'undefined'){
+					//already exists check to see if the function is already bound
+					eventStack = action.eventStore[eventName];
+
+					for(i = 0; i < eventStack.length; i ++){
+						if(eventStack[i] === functionIn){
+							newCheck = false;
+							break;
+						}
+					}
+
+					console.log(newCheck);
+
+					if(newCheck){
+						eventStack.push(functionIn);
+					}
+
+				} else{
+					//new event
+					action.eventStore[eventName] = []; //use an array to store functions
+					action.eventStore[eventName].push(functionIn);
 				}
-
-			} else{
-				//new event
-				that.eventStore[eventName] = []; //use an array to store functions
-				that.eventStore[eventName].push(functionIn);
 			}
+
+			returnFunction.silence = function(eventName, functionIn){
+				var i;
+
+				for(i = 0; i < action.eventStore[eventName]; i ++){
+					if(functionIn === action.eventStore[eventName][i]){
+						action.eventStore[eventName].splice(i,1)
+					}
+				}
+			}
+
+			return returnFunction;
 		}
 
-		Function.prototype.silence = function(functionIn){
+		, eventStore: {}
+	};
 
-		}
-	}
+	action.events = action.newFunction(function(){})
 
-	, events: function()}{}
-
-	, eventStore: {}
-};
-
-action.init();
+	return action;
+}(this);
