edcf5b64c42a599da23d8831c55dcccb3711e471
Daniel Sellers <d.sellers@ldschurch.org>
2014-04-29 09:51:02 -0600

    added a publish to the gulp file, revved the package.json, and built out version 0.2.0

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,11 @@
 var gulp = require('gulp')
     , watch = require('gulp-watch')
     , plumber = require('gulp-plumber')
-    , concat = require('gulp-concat');
+    , concat = require('gulp-concat')
+    , uglify = require('gulp-uglify')
+    , rename = require('gulp-rename')
+
+    , pkg = require('./package.json');
 
 gulp.task('default', ['localBuild'], function(){
     'use strict';
@@ -19,6 +23,16 @@ gulp.task('localBuild', function(){
         .pipe(gulp.dest('public/javascripts/built/'));
 });
 
+gulp.task('publish', function(){
+    'use strict';
+
+    gulp.src('public/javascripts/action.js')
+        .pipe(rename('action-v' + pkg.version + '.js'))
+        .pipe(gulp.dest('packages/' + pkg.version + '/'))
+        .pipe(uglify())
+        .pipe(rename('action-v' + pkg.version + '.min.js'))
+        .pipe(gulp.dest('packages/' + pkg.version + '/'));
+});
 
 // gulp.task('default', function () {
 //     watch({ glob: ['public/javascripts/app.js', 'public/javascripts/components/**/*.js'], emitOnGlob: false })
