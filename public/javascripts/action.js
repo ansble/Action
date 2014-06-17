b4823a6de9a7b06c9ce98732a6946666e9cc4232
Daniel Sellers <d.sellers@ldschurch.org>
2014-06-17 10:24:15 -0600

    added an event for domReady

--- public/javascripts/action.js
+++ public/javascripts/action.js
@@ -695,6 +695,10 @@ var action = function(){
         // throw errorIn;
     });
 
+    document.addEventListener("DOMContentLoaded", function(){
+        action.emit('dom:ready');
+    });
+
     //return the tweaked function
     return action;
 }(this);
