1e10fe808787021328388481065b321baafaae6d
Daniel Sellers <daniel@designfrontier.net>
2014-11-12 21:01:02 -0700

    now building with browserify from modules

--- src/cjs/action.js
+++ src/cjs/action.js
@@ -2,56 +2,53 @@ var eventMe = require('./action.events')
 	, viewMe = require('./action.view')
 	, modelMe = require('./action.model')
 	, utils = require('./action.utils')
-	, routeMe = require('./action.route')
+	, routeMe = require('./action.route');
 
-	, action = eventMe({
-		eventStore: {}
-		, eventMe: eventMe
-		, routeMe: routeMe
-		, viewMe: viewMe
-		, clone: utils.clone
-		, Error: utils.Error
-	});
+window.action = {
+	eventStore: {}
+	, eventMe: eventMe
+	, routeMe: routeMe
+	, viewMe: viewMe
+    , modelMe: modelMe
+	, clone: utils.clone
+	, Error: utils.Error
+    , init: function(){
+        'use strict';
 
-action.listen('template:get', function(templateID){
-    'use strict';
+        this.listen('template:get', function(templateID){
+            this.emit('template:set:' + templateID, this.templates[templateID]);
+        });
 
-    action.emit('template:set:' + templateID, action.templates[templateID]);
-});
+        this.listen('global:error', function(errorIn) {
+            
+            console.group('An Error occured in an object with emitterid: ' + errorIn.createdBy.emitterId);
+            console.log('It was a ' + errorIn.type + 'error.');
 
-action.listen('global:error', function(errorIn) {
-    'use strict';
-    
-    console.group('An Error occured in an object with emitterid: ' + errorIn.createdBy.emitterId);
-    console.log('It was a ' + errorIn.type + 'error.');
+            if(typeof errorIn.errorObject === 'string'){
+                console.log('It says: ' + errorIn.errorObject);
+                console.log('and: ' + errorIn.message);
+            } else {
+                console.log('It says: ' + errorIn.message);
+            }
 
-    if(typeof errorIn.errorObject === 'string'){
-        console.log('It says: ' + errorIn.errorObject);
-        console.log('and: ' + errorIn.message);
-    } else {
-        console.log('It says: ' + errorIn.message);
+            console.log('The Whole Enchilada (object that caused this mess):');
+            console.dir(errorIn.createdBy);
+
+            if(typeof errorIn.createdBy.flatten === 'function'){
+                console.log('Just the Lettuce (attributes):');
+                console.dir(errorIn.createdBy.flatten());
+            }
+
+            if(typeof errorIn.errorObject === 'object'){
+                console.log('Oh look an Error Object!:');
+                console.dir(errorIn.errorObject);
+            }
+
+            console.groupEnd();
+            // this.trace(errorIn.createdBy.emitterId);
+            // throw errorIn;
+        });
     }
+};
 
-    console.log('The Whole Enchilada (object that caused this mess):');
-    console.dir(errorIn.createdBy);
-
-    if(typeof errorIn.createdBy.flatten === 'function'){
-        console.log('Just the Lettuce (attributes):');
-        console.dir(errorIn.createdBy.flatten());
-    }
-
-    if(typeof errorIn.errorObject === 'object'){
-        console.log('Oh look an Error Object!:');
-        console.dir(errorIn.errorObject);
-    }
-
-    console.groupEnd();
-    // action.trace(errorIn.createdBy.emitterId);
-    // throw errorIn;
-});
-
-if(typeof window !== 'undefined'){
-    window.action = action;
-} else {
-    module.exports = action;
-}
+window.action = eventMe(window.action);
